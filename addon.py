c31160c231ca2ca338fbfa5e6152cf504dfc043f
esxbr <patrick.cabral@gmail.com>
2015-04-24 12:36:15 -0400

    first

---
+++ addon.py
@@ -0,0 +1,75 @@
+﻿import sys
+import os
+import json
+import urllib
+import urlparse
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+import load_channels
+
+
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+addondir    = xbmc.translatePath( addon.getAddonInfo('profile') ) 
+
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+
+
+#xbmcgui.Dialog().ok(addonname, 'aaa')
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+
+mode = args.get('mode', None)
+
+stalker_url = args.get('stalker_url', None)
+
+if stalker_url is None:
+	stalker_url = xbmcplugin.getSetting(int(sys.argv[1]), "url")
+else:
+	stalker_url = stalker_url[0];
+	
+def build_url(query):
+	return base_url + '?' + urllib.urlencode(query)
+
+if mode is None:
+	
+	data = load_channels.getAllChannels(stalker_url, addondir);
+	data = data['channels'];
+	
+
+	for i in data:
+		name 	= i["name"];
+		cmd 	= i["cmd"];
+		tmp 	= i["tmp"];
+		number 	= i["number"];
+		
+		url = build_url({'mode': 'folder', 'stalker_url' : stalker_url, 'cmd': cmd, 'tmp' : tmp, 'number' : number, 'title' : name.encode("utf-8")});
+				
+		li = xbmcgui.ListItem(name, iconImage='DefaultVideo.png')
+		#li.setInfo(type='Video', infoLabels={ "Title": title })
+		#li.setProperty('IsPlayable', 'true')
+
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+elif mode[0] == 'folder':
+	
+	title 	= args['title'][0];
+	number 	= args['number'][0];
+	cmd 	= args['cmd'][0];
+	tmp 	= args['tmp'][0];
+	
+	url = load_channels.retriveUrl(stalker_url, cmd, tmp);
+	
+	li = xbmcgui.ListItem(label=title, path=url)
+	xbmc.Player().play(item=url, listitem=li)
+
+
+
+xbmcplugin.endOfDirectory(addon_handle)
+
+
+	
---
+++ load_channels.py
@@ -0,0 +1,165 @@
+import sys
+import urllib
+import urllib2
+import json
+import os
+import urlparse
+import re, uuid
+from time import time
+from datetime import datetime
+
+key=''
+
+def handshake(url):
+	global key;
+	
+	info = retrieveData(url, values = {
+		'type' : 'stb', 
+		'action' : 'handshake',
+		'JsHttpRequest' : '1-xml'})
+
+	key = info['js']['token']
+	
+	#print 'key: ' + key
+
+
+def retrieveData(url, values ):
+	global key;
+	
+	mac = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+
+	url += '/stalker_portal'
+	load = '/server/load.php'
+	refer = '/c/'
+	timezone = 'Europe%2FKiev';
+
+	user_agent 	= 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3';
+	
+	if key != '':
+		#print 'Authorization Bearer ' + key;
+		
+		headers 	= { 
+			'User-Agent' : user_agent, 
+			'Cookie' : 'mac=' + mac+ '; stb_lang=en; timezone=' + timezone,
+			'Referer' : url + refer,
+			'Accept' : '*/*',
+			'X-User-Agent' : 'Model: MAG250; Link: WiFi',
+			'Authorization' : 'Bearer ' + key };
+	
+	else:
+		headers 	= { 
+			'User-Agent' : user_agent, 
+			'Cookie' : 'mac=' + mac+ '; stb_lang=en; timezone=' + timezone,
+			'Referer' : url + refer,
+			'Accept' : '*/*',
+			'X-User-Agent' : 'Model: MAG250; Link: WiFi' };
+		
+
+
+	data = urllib.urlencode(values);
+	req = urllib2.Request(url + load, data, headers);
+	data = urllib2.urlopen(req).read().decode("utf-8");
+	info = json.loads(data)
+
+	return info;
+
+
+def getAllChannels(url, path ):
+
+	now = time();
+
+	if os.path.exists(path + '/channels.json'):
+		#check last time
+		with open(path + '/channels.json') as data_file: data = json.load(data_file);
+	
+		time_init = float(data['time']);
+	
+		# update 12h
+		if ((now - time_init) / 3600) < 12:
+			return data;
+	
+	
+	handshake(url);
+	
+	info = retrieveData(url, values = {
+		'type' : 'itv', 
+		'action' : 'get_all_channels',
+		'JsHttpRequest' : '1-xml'})
+	
+	results = info['js']['data']
+	
+	#print results;
+
+	data = '{ "time" : "' + str(now) + '", "channels" : [ \n'
+
+	for i in results:
+		number 	= i["number"]
+		name 	= i["name"]
+		cmd 	= i['cmd']
+		logo 	= i["logo"]
+		tmp 	= i["use_http_tmp_link"]
+		
+		data += '{"number":"'+ number +'", "name":"'+ name +'", "cmd":"'+ cmd +'", "logo":"'+ logo +'", "tmp":"'+ str(tmp) +'"}, \n'
+
+	
+	data = data[:-3] + '\n]}'
+
+	with open(path + '/channels.json', 'w') as f: f.write(data.encode('utf-8'));
+	
+	return json.loads(data.encode('utf-8'));
+
+def retriveUrl(url, channel, tmp):
+	
+	cmd = channel;
+	
+	if tmp != "0":
+		info = retrieveData(url, values = {
+			'type' : 'itv', 
+			'action' : 'create_link', 
+			'cmd' : channel,
+			'forced_storage' : 'undefined',
+			'disable_ad' : '0',
+			'JsHttpRequest' : '1-xml'});
+		cmd = info['js']['cmd'];
+		
+	s = cmd.split(' ');
+			
+	url = s[0];
+	
+	if len(s)>1:
+		url = s[1];
+		
+	#print url;
+
+	if tmp != "0":
+		# RETRIEVE THE 1 EXTM3U
+		request = urllib2.Request(url)
+		request.get_method = lambda : 'HEAD'
+		response  = urllib2.urlopen(request);
+		data = response.read().decode("utf-8");
+		data = data.splitlines();
+		data = data[len(data) - 1];
+	
+		# RETRIEVE THE 2 EXTM3U
+		url = response.geturl().split('?')[0];
+		url_base = url[: -(len(url) - url.rfind('/'))]
+		return url_base + '/' + data;
+	else:
+		return url;
+
+
+
+def main(argv):
+
+      if argv[0] == 'load':
+      	getAllChannels(argv[1], argv[2]);
+      	
+      elif argv[0] == 'channel':
+      	url = retriveUrl(argv[1], argv[2], argv[3]);
+      	print url
+      	#os.system('/Applications/VLC.app/Contents/MacOS/VLC ' + url)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
